152044ae213ba278f04498481ef9bc495517f15a
docinho <livia.juliao@ccc.ufcg.edu.br>
2018-06-10 22:26:16 -0300

    terminando programinha

--- haskell/twitter.hs
+++ haskell/twitter.hs
@@ -1,4 +1,8 @@
+-- Crie um programa que receba uma frase como entrada e imprima quantos caracteres restam até atingir o limite de 140 caracteres do twitter. 
+
 main = do
 	msg <- getLine
 	let c = length msg
-	print c
+	let d =  140 - c
+	print d
+
